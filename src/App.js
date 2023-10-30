3582852fef117e4e3601af4d70c475bd5cade805
lKrollo <rolf04@live.dk>
2023-10-30 16:39:51 +0100

    Show all pokémon and pagination feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PokemonList from './Components/PokemonList';
+import PokemonDetails from './Components/PokemonDetails';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <PokemonList />
+        </div>
+    );
 }
 
 export default App;
