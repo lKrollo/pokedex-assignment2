a88a8ca5e07bf92ac9aaeed7c2d369475adb1a69
lKrollo <rolf04@live.dk>
2023-11-02 08:50:31 +0100

    Window with details about the pokemon can be opened

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PokeDex from './Components/Pokédex';
 import logo from './img/pokedexLogo.svg'
 
 function App() {
+    const [page, setPage] = useState(1)
+
+    const handleLogoClick = () => {
+        // Set the page to 1 when the logo is clicked
+        setPage(1);
+    };
+
     return (
         <div>
-            <img id="logo" src={logo} alt="Logo" />
+            <img id="logo" src={logo} alt="Logo" onClick={handleLogoClick} />
             <PokeDex />
         </div>
     );
